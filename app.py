3fc121fe1ddf00440137b50ac11e1da1f0858a15
Regis <r.petrauskui@gmail.com>
2023-08-21 09:34:06 +0300

    Sukuriame puslapy 'orai'

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/orai")
+def ora():
+    return render_template('orai.html')
+
 if __name__ == "__main__":
     app.run()
