784674f41e1af1fa51cf77a5d39483ffc3c33968
Regis <r.petrauskui@gmail.com>
2023-08-21 09:48:26 +0300

    sukuriame keleta puslapiu ir end pointu

--- app.py
+++ app.py
@@ -10,5 +10,24 @@ def home():
 def ora():
     return render_template('orai.html')
 
+@app.route("/naujienos")
+def naujienos():
+    return render_template('naujienos.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('forma.html')
+    if request.method == 'POST':
+        vardas = request.form['laukelis']
+        return render_template('vardas.html', sablono_kint=vardas)
+    else:
+        return render_template('forma.html')
+
+@app.route('/<kintamasis>')
+def user(kintamasis):
+    return render_template('vardas.html', sablono_kint=kintamasis)
+
 if __name__ == "__main__":
     app.run()
